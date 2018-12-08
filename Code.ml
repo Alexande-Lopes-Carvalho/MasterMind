9cf8ce15f5e85cb2c2ed96256f5308e98b89dee9
Alexandre Lopes Carvalho <alexandre.lopes.carvalho@eisti.fr>
2018-12-08 11:32:35 +0100

    Update Code.ml

--- Code.ml
+++ Code.ml
@@ -9,10 +9,10 @@ module Code :
     val nombre_pions : int
     (* motif pour affichage console de pion bien placé *)
 
-    val reponseBonneMotif  : string
+    val wellplacedMotif  : string
     (* motif pour affichage console de pion mal placé *)
 
-    val reponseMauvaiseMotif : string
+    val misplacedMotif : string
     (** Liste des couleurs possibles *)
 
     val couleurs_possibles : pion list
@@ -59,6 +59,9 @@ module Code :
     (* a supp pour debug *)
     val makeCode : string -> t
 
+    (* affiche une liste de code*)
+    val printListCode : t list -> unit
+
     (** La liste de tous les codes permis *)
     val tous : t list
 
@@ -84,8 +87,8 @@ module Code :
       let pionToken = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'];;
       let pionColor = ["[0;31m"; "[0;32m"; "[0;33m"; "[0;34m"; "[0;35m"; "[0;36m"; "[0;37m"; "[1;31m"; "[1;32m"; "[1;33m"; "[1;34m"; "[1;35m"; "[1;36m"; "[1;37m"];;
       let defaultColor = "[0m";;
-      let reponseBonneMotif = "/";;
-      let reponseMauvaiseMotif = ".";;
+      let wellplacedMotif = "/";;
+      let misplacedMotif = ".";;
 
       let rec makeArray n l =
         if n > 0 then makeArray (n-1) ((Color(n-1))::l)
@@ -114,11 +117,14 @@ module Code :
 
       let string_of_pion a =
         let v = extractColor a in
-        (fill (List.nth pionColor v) (*^ " "*)) ^ (String.make 1 (List.nth pionToken v))
+        (fill (List.nth pionColor v) (*^ " "*)) ^ (String.make 1 (List.nth pionToken v)) ^ (fill defaultColor)
       ;;
 
       let string_of_code t =
-          (List.fold_left (fun acc c -> acc ^ (string_of_pion c)) "" t) ^ (fill defaultColor)
+          (List.fold_left (fun acc c -> let v = extractColor c in 
+                                        acc ^ (fill (List.nth pionColor v)) ^ (String.make 1 (List.nth pionToken v))
+                           ) "" t
+                          ) ^ (fill defaultColor)
         ;;
 
       let rec charlist_of_string_rec s i l =
@@ -153,13 +159,18 @@ module Code :
       let string_of_reponse x =
         match x with
         | None -> ""
-        | Some(y) -> (build (fst(y)) "" reponseBonneMotif) ^ (build (snd(y)) "" reponseMauvaiseMotif)
+        | Some(y) -> (build (fst(y)) "" wellplacedMotif) ^ (build (snd(y)) "" misplacedMotif)
       ;;
 
       let makeCode s = (* Pour Debug*)
         match code_of_string s with 
         | Some(x) -> x
         | None -> [Color(0)]
+      ;;
+
+      let printListCode l = 
+        Sys.command "clear";
+        print_endline ((List.fold_left (fun acc c -> acc ^ "[" ^ (string_of_code c) ^ "] ") "[ " l) ^ "]");
       ;;
 
       let tous =
@@ -174,7 +185,7 @@ module Code :
           if n > 0 then makeArray (n-1) ((n-1)::l)
           else l
         in let o = makeArray (nombre_pions+1) [] in
-        List.fold_left (fun acc c -> acc@(List.fold_left (fun acc_ c_ -> if c+c_ <= nombre_pions then acc_@[(c, c_)] else acc_ 
+        List.fold_left (fun acc c -> acc@(List.fold_left (fun acc_ c_ -> if c+c_ <= nombre_pions && ((c = nombre_pions-1 && c_ = 0) || (c <> nombre_pions-1)) then acc_@[(c, c_)] else acc_ 
                                                           ) [] o)
                         ) [] o
       ;;
@@ -255,6 +266,8 @@ print_endline ("Compare " ^ (string_of_int (Code.compare (Code.makeCode"abaf") (
 print_endline ("Compare " ^ (string_of_int (Code.compare (Code.makeCode"bbaf") (Code.makeCode"abaf") )));;
 print_endline ("Compare " ^ (string_of_int (Code.compare (Code.makeCode"afff") (Code.makeCode"baaa") )));;
 print_endline ("Compare " ^ (string_of_int (Code.compare (Code.makeCode"aaba") (Code.makeCode"aaaa") )));;
+
+Code.printListCode [Code.makeCode"aaba"; Code.makeCode"aaba"; Code.makeCode"aaba"; Code.makeCode"aaba"; Code.makeCode"aaba"];
 
 (*
 let reponse a b =
