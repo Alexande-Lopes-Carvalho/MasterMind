fe2c4191cc408f35647c98f05d0637d3562f85b5
Alexandre Lopes Carvalho <alexandre.lopes.carvalho@eisti.fr>
2018-12-08 11:33:03 +0100

    Update IA.ml

--- IA.ml
+++ IA.ml
@@ -29,24 +29,68 @@ module IA :
     struct
     let nombre_methodes = 2;; (* de 0 a 1*)
 
-    let choix ch listEssaiPropose listEssaiPossible =
-      match ch with
-      | 0 -> algoNaif listEssaiPossible
-      |
-    ;;
-
     let algoNaif listEssaiPossible =
       match listEssaiPossible with
       | x::l -> x
+      | _ -> failwith "ERROR AT algoNaif | listEssaiPossible Empty"
     ;;
-
-    let filtre ch listEssaiPropose listEssaiPossible =
+    
+    let choix ch listEssaiPropose listEssaiPossible =
       match ch with
-      | 0 -> filtreNaif listEssaiPossible
+      | 0 -> algoNaif listEssaiPossible
+      | _ -> algoNaif listEssaiPossible
     ;;
 
-    let filtreNaif listEssaiPossible =
-      match l with
-      | x :: k -> x
+    let eraseUnrelatedStrict l code = (* Supprime les code n'ayant pas strictment les meme couleurs *)
+       List.fold_left (fun acc c -> if List.length ((List.fold_left (fun acc_ c_ -> (*printList acc_;*) fst (List.fold_left (fun acc__ c__ -> let i = snd acc__ in
+                                                                                                                                              if i then ((fst acc__)@[c__], i) 
+                                                                                                                                              else if c__ <> c_ then ((fst acc__)@[c__], i) 
+                                                                                                                                              else (fst acc__, true)
+                                                                                                                             ) ([], false) acc_)
+                                                                     ) c code)) = 0 then acc@[c] else acc  
+                        ) [] l
+    ;;
+
+    let eraseRelatedStrict l code = (* Supprime les code ayant strictment les meme couleurs *)
+      List.fold_left (fun acc c -> if List.length ((List.fold_left (fun acc_ c_ -> (*printList acc_;*) fst (List.fold_left (fun acc__ c__ -> let i = snd acc__ in
+                                                                                                                                             if i then ((fst acc__)@[c__], i) 
+                                                                                                                                             else if c__ <> c_ then ((fst acc__)@[c__], i) 
+                                                                                                                                             else (fst acc__, true)
+                                                                                                                            ) ([], false) acc_)
+                                                                     ) c code)) <> 0 then acc@[c] else acc  
+                      ) [] l
+    ;;
+
+    let eraseUnrelated l code = (* Supprime les code ayant aucune couleur dans le code *)
+    List.fold_left (fun acc c -> if (List.fold_left (fun acc_ c_ -> acc_ || List.fold_left (fun acc__ c__ -> acc__ || c__ = c_ 
+                                                                                            ) false c 
+                                                     ) false code) then acc@[c]
+                                 else acc
+                    ) [] l
+    ;;
+
+    let eraseRelated l code = (* Supprime les code ayant une couleur dans le code *)
+      List.fold_left (fun acc c -> if not(List.fold_left (fun acc_ c_ -> acc_ || List.fold_left (fun acc__ c__ -> acc__ || c__ = c_ 
+                                                                                              ) false c 
+                                                       ) false code) then acc@[c]
+                                   else acc
+                      ) [] l
+      ;;
+
+    let filtreNaif reponse listEssaiPossible =
+      Code.printListCode listEssaiPossible;
+      match listEssaiPossible with 
+      | code::l -> match (snd reponse) with 
+                  | Some(x) -> if (fst x + snd x = Code.nombre_pions) then eraseUnrelatedStrict l (fst reponse)
+                               else if (fst x + snd x <> 0) then eraseRelatedStrict (eraseUnrelated l (fst reponse)) (fst reponse)
+                               else eraseRelated l (fst reponse)
+                  | None -> listEssaiPossible
+      | _ -> listEssaiPossible
+    ;;
+
+    let filtre ch reponse listEssaiPossible =
+      match ch with
+      | 0 -> filtreNaif reponse listEssaiPossible
+      | _ -> filtreNaif reponse listEssaiPossible
     ;;
 end;;
