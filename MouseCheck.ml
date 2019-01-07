e0827a86c1432d3b9bf45c125a4418f210a3af8f
Mehdi Boukelmoun <mehdi.boukelmoun@eisti.fr>
2019-01-07 12:18:39 +0100

    Update 2 MouseCheck.ml

--- MouseCheck.ml
+++ MouseCheck.ml
@@ -1,11 +1,23 @@
 #load "graphics.cma";;
+
 let width = ref 200;;
 let height = ref 200;;
+
 
 let size x y =
       Graphics.open_graph (" " ^ (string_of_int x)^"x"^(string_of_int y));
       Graphics.set_font "-*-fixed-medium-r-semicondensed--25-*-*-*-*-*-iso8859-1";
 	  Graphics.auto_synchronize false;
 ;;
+
+let color r g b = Graphics.rgb r g b;;
+
+let fill co = Graphics.set_color (co);;
+
+let background co =
+  fill co;
+  Graphics.fill_rect 0 0 !width !height
+;;
+
 
 let exit () = Graphics.close_graph ();;
